4fa79e482a8d33ecfd5b96796ad4d5826953e4e1
Jamyle Elen <jamyleelen09@gmail.com>
2024-05-16 22:29:23 -0300

    Create atividades.js

---
+++ atv START/Atividade 1 2 3 4 5/atividades.js
@@ -0,0 +1,46 @@
+
+// Soma de números inteiros
+let valor1 = 2;
+let valor2 = 4;
+let soma = valor1 + valor2;
+console.log(`A soma de ${valor1} + ${valor2} é igual a: ${soma}`);
+
+// Média aluno
+let n1 = 2;
+let n2 = 3;
+
+let media = (n1+n2) / 2;
+console.log(`Suas notas foram ${n1} e ${n2}`);
+console.log(`Sua média é: ${media}`);
+
+// Dobro e terça parte do valor
+
+let dobro = n1 * 2;
+let tu = n1 / 3;
+console.log(n1);
+console.log(`O dobro desse valor é ${dobro}`);
+console.log(`Sua terça parte é: ${tu}`);
+
+// Área a ser pintada
+
+let width = 4;
+let height = 2;
+let areaPintada = width * height;
+
+console.log(`Altura ${height} metros`);
+console.log(`Largura ${width} metros`);
+console.log(`Área a ser pintaaa: ${areaPintada} metros`);
+console.log(`São necessáros ${areaPintada/2} L de tinta`);
+
+// Valor gasto em alugel de carro
+
+let diariaAlugel = 90;
+let valorPorKm = 0.20;
+let kmPercorrido = 34;
+let diasAlugado = 3;
+let gastoTotal = ((diariaAlugel * diasAlugado) + (valorPorKm * kmPercorrido));
+
+console.log(`Valor diária: ${diariaAlugel}`);
+console.log(`Valor por KM: ${valorPorKm}`);
+console.log(`Dias: ${diasAlugado}`);
+console.log(`Você gastou R$ ${gastoTotal.toFixed(2)} com esse aluguel`);
