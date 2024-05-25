5ab12b2add2e2267299c825fbde471b62ad86788
Jamyle Elen <jamyleelen09@gmail.com>
2024-05-25 07:24:00 -0300

    atividade

--- 23-05 Atividade 3/while.js
+++ 23-05 Atividade 3/while.js
@@ -1,11 +1,24 @@
 // Soma entre os números digitados
+// let numbers = [];
+
+// for (let i = 0; i < 10; i++) {
+//     let num = parseInt(prompt("Digite um número: "));
+//     numbers.push(num);
+// }
+
+// let sumNumbers = numbers.reduce((acc, curr) => acc + curr, 0);
+// alert(`Soma dos números.: ${sumNumbers}`);
+
+//
 let numbers = [];
 
-for (let i = 0; i < 10; i++) {
-    let num = parseInt(prompt("Digite um número:"));
+for(let i = 0; i < 100; i++) {
+    let num = parseInt(prompt("Digite um número: "));
+    if (num === 0) {
+        break;
+    }
     numbers.push(num);
 }
 
 let sumNumbers = numbers.reduce((acc, curr) => acc + curr, 0);
-
-alert(`Soma dos números.: ${sumNumbers}`);
+alert(`Soma dos números: ${sumNumbers}`);
