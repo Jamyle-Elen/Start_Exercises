b1856c4e373894349f436ba7604b139ed60654c6
Jamyle Elen <jamyleelen09@gmail.com>
2024-05-25 07:05:19 -0300

    atividade

---
+++ 23-05 Atividade 3/while.js
@@ -0,0 +1,10 @@
+let numbers = [];
+
+for (let i = 0; i < 10; i++) {
+    let num = parseInt(prompt("Digite um número:"));
+    numbers.push(num);
+}
+
+let sumNumbers = numbers.reduce((acc, curr) => acc + curr, 0);
+
+alert(`Soma dos números.: ${sumNumbers}`);
