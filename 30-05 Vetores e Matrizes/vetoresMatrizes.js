261525c97cefa382799cf61cc7bdcc88720be7e7
Jamyle Elen <jamyleelen09@gmail.com>
2024-06-02 09:01:57 -0300

    vetores e matrizes

--- 30-05 Vetores e Matrizes/vetoresMatrizes.js
+++ 30-05 Vetores e Matrizes/vetoresMatrizes.js
@@ -7,13 +7,26 @@
 // console.log(`Números aleatórios ${numbers}`);
 
 // 2. Faça um programa que leia 7 nomes de pessoas e guarde-os em um vetor. No final, mostre uma listagem com todos os nomes informados, na ordem inversa daquela em que eles foram informados.
-let names = [];
-for (let i = 0; i < 7; i++) {
-    let name = prompt(`Digite o nome da ${i +1}º pessoa: `);
-    names.push(name);
-}
+// let names = [];
+// for (let i = 0; i < 7; i++) {
+//     let name = prompt(`Digite o nome da ${i +1}º pessoa: `);
+//     names.push(name);
+// }
 
-console.log("Ordem inversa dos nomes: ");
-for (let i = names.length - 1; i >=0; i--) {
-    console.log(names[i]);
+// console.log("Ordem inversa dos nomes: ");
+// for (let i = names.length - 1; i >=0; i--) {
+//     console.log(names[i]);
+// }
+
+// 3. Escreva um programa que leia 15 números e guarde-os em um vetor. No final, mostre o vetor inteiro na tela e em seguida mostre em que posições foram digitados valores que são múltiplos de 10.
+let numbers = [];
+for (let i = 0; i < 15; i++) {
+    let number = parseInt(prompt(`Digite o ${i + 1}º número: `));
+    numbers.push(number);
+}
+console.log(`Números digitados: ${numbers}`);
+
+console.log("Posições que são múltiplas de 10: ");
+for (let i =0; i < numbers.length; i++) {
+    numbers[i] % 10 === 0 ? console.log(`Valor ${numbers[i]} na ${i}º posição`) : null;
 }
