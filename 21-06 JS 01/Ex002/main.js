7f0b9bebac00b32bf4a0236a9f54186037851858
Jamyle Elen <jamyleelen09@gmail.com>
2024-06-24 21:18:24 -0300

    feat: add function to check if a number is even or odd

---
+++ 21-06 JS 01/Ex002/main.js
@@ -0,0 +1,6 @@
+// Desenvolva uma função que receba um número como parâmetro e verifique se ele é par ou ímpar. Retorne true se for par e false se for ímpar.
+function oddOrEven(number) {
+    return number % 2 === 0 ? console.log('True') : console.log('False')
+}
+oddOrEven(4);
+oddOrEven(3);
