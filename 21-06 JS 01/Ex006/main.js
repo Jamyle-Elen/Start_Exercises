41bcfb9e1b556b5735bfe6173aea2d492e1c17f3
Jamyle Elen <jamyleelen09@gmail.com>
2024-06-24 22:45:45 -0300

    feat: function that reverses the order of elements within an array

---
+++ 21-06 JS 01/Ex006/main.js
@@ -0,0 +1,10 @@
+// Implemente uma função que inverta a ordem dos elementos em um array. Utilize essa função para inverter a ordem de diferentes conjuntos de elementos.
+let box = [2, 4, 6, 8,]
+let box2 = [4, 44, 444, 4444]
+
+function reverseElements(array) {
+    return array.reverse()
+}
+
+console.log(reverseElements(box))
+console.log(reverseElements(box2))
