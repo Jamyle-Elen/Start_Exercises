2c51cc43a30b1133e7eb9851aeb0ed75467db584
Jamyle Elen <jamyleelen09@gmail.com>
2024-07-03 18:22:25 -0300

    feat: homepage

--- 03-07 Mini Projeto/project/src/App.jsx
+++ 03-07 Mini Projeto/project/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import imageHome from '../src/assets/img/image-home.png'
+import MenuBar from './components/menuBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <MenuBar/>
+      <main>
+        <section className='home'>
+          <div className="start">
+            <h1>Tornando sua empresa digital com <span>ideias inovadoras.</span></h1>
+            <p>Uma mistura de Design, Desenvolvimento Frontend e habilidade profissional que vão destacar seu produto no mercado.</p>
+            <button>Instagram</button>
+          </div>
+          <div className="end">
+            <img src={imageHome} alt="" />
+          </div>
+        </section>
+      </main>
+
     </>
   )
 }
